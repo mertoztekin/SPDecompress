aadac8eec1c9837d477788847f5d2491cee96467
mertoztekin <mydonose@gmail.com>
2013-07-02 23:15:25 +0300

    First commit

---
+++ SPDecompress/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace SPDecompress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxCompressed = new System.Windows.Forms.TextBox();
+            this.webBrowserUncompressed = new System.Windows.Forms.WebBrowser();
+            this.buttonDecompress = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.labelStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtOuput = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxCompressed
+            // 
+            this.textBoxCompressed.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxCompressed.Location = new System.Drawing.Point(12, 12);
+            this.textBoxCompressed.Multiline = true;
+            this.textBoxCompressed.Name = "textBoxCompressed";
+            this.textBoxCompressed.Size = new System.Drawing.Size(497, 130);
+            this.textBoxCompressed.TabIndex = 0;
+            // 
+            // webBrowserUncompressed
+            // 
+            this.webBrowserUncompressed.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowserUncompressed.Location = new System.Drawing.Point(3, 3);
+            this.webBrowserUncompressed.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserUncompressed.Name = "webBrowserUncompressed";
+            this.webBrowserUncompressed.Size = new System.Drawing.Size(483, 226);
+            this.webBrowserUncompressed.TabIndex = 1;
+            // 
+            // buttonDecompress
+            // 
+            this.buttonDecompress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDecompress.Location = new System.Drawing.Point(434, 148);
+            this.buttonDecompress.Name = "buttonDecompress";
+            this.buttonDecompress.Size = new System.Drawing.Size(75, 23);
+            this.buttonDecompress.TabIndex = 2;
+            this.buttonDecompress.Text = "Decompress";
+            this.buttonDecompress.UseVisualStyleBackColor = true;
+            this.buttonDecompress.Click += new System.EventHandler(this.buttonDecompress_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Paste From Clipboard";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.labelStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 439);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(521, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.ForeColor = System.Drawing.Color.Red;
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(506, 17);
+            this.labelStatus.Spring = true;
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 178);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(497, 258);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.webBrowserUncompressed);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(489, 232);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "XML";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtOuput);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(489, 232);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Text";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtOuput
+            // 
+            this.txtOuput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOuput.Location = new System.Drawing.Point(3, 3);
+            this.txtOuput.Multiline = true;
+            this.txtOuput.Name = "txtOuput";
+            this.txtOuput.Size = new System.Drawing.Size(483, 226);
+            this.txtOuput.TabIndex = 0;
+            this.txtOuput.DoubleClick += new System.EventHandler(this.txtOuput_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 461);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonDecompress);
+            this.Controls.Add(this.textBoxCompressed);
+            this.Name = "Form1";
+            this.Text = "SPDecompress";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxCompressed;
+        private System.Windows.Forms.WebBrowser webBrowserUncompressed;
+        private System.Windows.Forms.Button buttonDecompress;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel labelStatus;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox txtOuput;
+    }
+}
+
---
+++ SPDecompress/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace SPDecompress
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDecompress_Click(object sender, EventArgs e)
+        {
+            byte[] compressedByteBuffer;
+            string uncompressedStringBuffer = String.Empty;
+
+            labelStatus.Text = String.Empty;
+
+            #region Prepare Byte Buffer
+
+            try
+            {
+                if (textBoxCompressed.Text.StartsWith("0x"))
+                {
+                    compressedByteBuffer = ConvertHexTringToByteArray(textBoxCompressed.Text.Substring(2));
+                }
+                else
+                {
+                    compressedByteBuffer = ConvertHexTringToByteArray(textBoxCompressed.Text);
+                }
+            }
+            catch (Exception exception)
+            {
+                labelStatus.Text = exception.Message;
+                return;
+            }
+
+            #endregion
+
+            #region Decompress Byte Buffer
+
+            try
+            {
+                uncompressedStringBuffer = Decompress(compressedByteBuffer);
+            }
+            catch (Exception exception)
+            {
+                labelStatus.Text = exception.Message;
+                return;
+            }
+
+            #endregion
+
+            #region Display Uncompressed Info
+            
+            XmlDocument xmlDocument = new XmlDocument();
+
+            xmlDocument.LoadXml("<uncompressedData></uncompressedData>");
+
+            bool isXmlDocument = false;
+
+            try
+            {
+                xmlDocument.FirstChild.InnerXml = uncompressedStringBuffer;                
+                isXmlDocument = true;
+            }
+            catch (Exception exception)
+            {
+                isXmlDocument = false;
+            }
+
+            txtOuput.Text = uncompressedStringBuffer;
+
+            if (isXmlDocument)
+            {
+                using (MemoryStream xslMemoryStream = new MemoryStream(Properties.Resources.DefaultStylesheet))
+                {
+                    XmlReader xmlReader = XmlReader.Create(xslMemoryStream);
+                    XslCompiledTransform xslCompiledTransform = new XslCompiledTransform();
+                    xslCompiledTransform.Load(xmlReader);
+                    StringBuilder xmlBuffer = new StringBuilder();
+                    XmlWriter xmlWriter = XmlWriter.Create(xmlBuffer);
+                    xslCompiledTransform.Transform(xmlDocument, xmlWriter);
+                    webBrowserUncompressed.DocumentText = xmlBuffer.ToString();
+                }
+            }
+            else
+            {
+                webBrowserUncompressed.DocumentText = uncompressedStringBuffer;
+            }
+
+            #endregion
+        }
+
+        private string Decompress(byte[] compressedBytesBuffer)
+        {
+            string uncompressedString = String.Empty;
+
+            using (MemoryStream compressedMemoryStream = new MemoryStream(compressedBytesBuffer))
+            {
+                compressedMemoryStream.Position += 12; // Compress Structure Header according to [MS -WSSFO2].
+                compressedMemoryStream.Position += 2;  // Zlib header.
+
+                using (DeflateStream deflateStream = new DeflateStream(compressedMemoryStream, CompressionMode.Decompress))
+                {
+                    using (MemoryStream uncompressedMemoryStream = new MemoryStream())
+                    {
+                        deflateStream.CopyTo(uncompressedMemoryStream);
+
+                        uncompressedMemoryStream.Position = 0;
+
+                        using (StreamReader streamReader = new StreamReader(uncompressedMemoryStream))
+                        {
+                            uncompressedString = streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return uncompressedString;
+        }
+
+
+        private byte[] ConvertHexTringToByteArray(string hexString)
+        {
+            int hexLength = hexString.Length;
+
+            if ((hexLength % 2) != 0)
+            {
+                throw new InvalidDataException("The number of characters in the sring should be even.");
+            }
+
+            byte[] hexValues = new byte[] {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
+
+            byte[] byteBuffer = new byte[hexLength / 2];
+
+            //e.g. ASCII value for 'A' = 65, substract ASCII value for '0' (48) => 17, locate 17th value in array.
+
+            for (int index = 0; index < hexLength; index += 2)
+            {
+                byteBuffer[index / 2] = (byte)(hexValues[Char.ToUpper(hexString[index + 0]) - '0'] << 4 |  hexValues[Char.ToUpper(hexString[index + 1]) - '0']);                
+            }
+
+            return byteBuffer;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                textBoxCompressed.Text = String.Empty;
+                textBoxCompressed.Text = Clipboard.GetText();
+            }
+        }
+
+        private void txtOuput_DoubleClick(object sender, EventArgs e)
+        {
+            txtOuput.SelectAll();
+        }
+    }
+}
